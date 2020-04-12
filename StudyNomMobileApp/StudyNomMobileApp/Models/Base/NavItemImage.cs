60dca714128ab220ba99666abb08b8af3aa9650d
AdilBikeev <bikeev1999@mail.ru>
2020-04-12 23:13:39 +0300

    develop Сделал шаблон для навигационной панели

---
+++ StudyNomMobileApp/StudyNomMobileApp/Models/Base/NavItemImage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace StudyNomMobileApp.Models.Base
+{
+    /// <summary>
+    /// Навиг. элементы.
+    /// </summary>
+    internal class NavItemImage
+    {
+        /// <summary>
+        /// Описание навиг. элемента (его имя).
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Путь к картинке навиг. элемента.
+        /// </summary>
+        public string Path { get; set; }
+
+        public NavItemImage (string title, string path)
+        {
+            this.Title = title;
+            this.Path = path;
+        }
+    }
+}
---
+++ StudyNomMobileApp/StudyNomMobileApp/Templates/Base/BottomNavigationBarView.cs
@@ -0,0 +1,91 @@
+﻿using StudyNomMobileApp.Hellpers;
+using StudyNomMobileApp.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+namespace StudyNomMobileApp.Templates.Base
+{
+    /// <summary>
+    /// Навигационная панель в нижней части приложения.
+    /// </summary>
+    class BottomNavigationBarView: ContentView
+    {
+        /// <summary>
+        /// Путь к каталогу с картинками.
+        /// </summary>
+        private const string pathImages = "Images/NavigationBar";
+
+        /// <summary>
+        /// Список элементов навигации.
+        /// </summary>
+        List<NavItemImage> navItemImages = new List<NavItemImage>()
+        {
+            new NavItemImage("Домой", "Nav_Home.png"),
+            new NavItemImage("Тренироваться", "Nav_Workout.png"),
+            new NavItemImage("Новости", "Nav_News.png"),
+            new NavItemImage("Статистика", "Nav_Stats.png")
+        };
+
+        public BottomNavigationBarView()
+        {
+            Content = this.GetContent();
+        }
+
+        /// <summary>
+        /// Возвращает контент для предсавления.
+        /// </summary>
+        /// <returns></returns>
+        private FlexLayout GetContent()
+        {
+            FlexLayout navBar = new FlexLayout()
+            {
+                Style = StyleHellpers.GetStyle<StackLayout>("NavFlex")
+            };
+
+            var Items = GetNavItems();
+            foreach (var view in Items)
+            {
+                navBar.Children.Add(view);
+            }
+
+            return navBar;
+        }
+
+        /// <summary>
+        /// Вовраащает список навигационных элементов в представлении StackLayout.
+        /// </summary>
+        private List<StackLayout> GetNavItems()
+        {
+            List<StackLayout> stackLayouts = new List<StackLayout>();
+
+            int n = this.navItemImages.Count;
+
+            foreach (var image in this.navItemImages)
+            {
+                StackLayout stackLayout = new StackLayout()
+                {
+                    Style = StyleHellpers.GetStyle<StackLayout>("NavStack")
+                };
+
+                stackLayout.Children.Add(new Image()
+                {
+                    Style = StyleHellpers.GetStyle<StackLayout>("NavBtns"),
+                    Source = Path.Combine(pathImages, image.Path)
+                });
+
+                stackLayout.Children.Add(new Label()
+                {
+                    Style = StyleHellpers.GetStyle<StackLayout>("NavLabels"),
+                    Text = image.Title
+                });
+
+                stackLayouts.Add(stackLayout);
+            }
+
+            return stackLayouts;
+        }
+    }
+}
