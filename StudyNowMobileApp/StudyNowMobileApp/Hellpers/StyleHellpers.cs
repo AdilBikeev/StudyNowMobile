07c976b601c8cba0185bfcaeb7d31f65ae1e1157
AdilBikeev <bikeev1999@mail.ru>
2020-04-13 08:11:12 +0300

    develop Произведено переименование проекта

---
+++ StudyNowMobileApp/StudyNowMobileApp/Hellpers/StyleHellpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace StudyNowMobileApp.Hellpers
+{
+    /// <summary>
+    /// Класс-помощник по заданию стилей.
+    /// </summary>
+    public static class StyleHellpers
+    {
+        /// <summary>
+        /// Возвращает стиль для указанного элемента управления.
+        /// </summary>
+        /// <typeparam name="T">Тип контрола.</typeparam>
+        /// <param name="baseResKey">Ключ словоря ресурсов, для автоматической подстанвоки стиля.</param>
+        /// <returns></returns>
+        public static Style GetStyle<T>(string baseResKey) => new Style(typeof(T))
+        {
+            BaseResourceKey = baseResKey
+        };
+    }
+}
---
+++ StudyNowMobileApp/StudyNowMobileApp/Templates/Base/ProfileIconView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ImageCircle.Forms.Plugin.Abstractions;
+using Xamarin.Forms;
+
+namespace StudyNowMobileApp.Templates.Base
+{
+    public class ProfileIconView : ContentView
+    {
+        public ProfileIconView()
+        {
+            Content = new CircleImage()
+            {
+                Style = new Style(typeof(ImageButton))
+                {
+                    BaseResourceKey = "ProfileTopBar"
+                }
+            };
+        }
+    }
+}
