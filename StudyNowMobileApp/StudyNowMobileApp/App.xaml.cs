77d2456eee78ed0ed5f79d9fcf6a4852d4a91b58
AdilBikeev <bikeev1999@mail.ru>
2020-04-15 00:15:10 +0300

    develop Сменил пространство имен для MainPage

--- StudyNowMobileApp/StudyNowMobileApp/App.xaml.cs
+++ StudyNowMobileApp/StudyNowMobileApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudyNowMobileApp.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
--- StudyNowMobileApp/StudyNowMobileApp/Views/MainPage.xaml.cs
+++ StudyNowMobileApp/StudyNowMobileApp/Views/MainPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace StudyNowMobileApp
+namespace StudyNowMobileApp.Views
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
