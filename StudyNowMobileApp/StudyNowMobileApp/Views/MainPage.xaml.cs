37c997e7dcb614992da3f81799dcc1def251fb4b
AdilBikeev <bikeev1999@mail.ru>
2020-04-15 12:51:13 +0300

    TabbedPage Переписал интерфейс на логику с BottomTabbedPage

--- StudyNowMobileApp/StudyNowMobileApp/Views/MainPage.xaml.cs
+++ StudyNowMobileApp/StudyNowMobileApp/Views/MainPage.xaml.cs
@@ -5,13 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Naxam.Controls.Forms;
 
 namespace StudyNowMobileApp.Views
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : BottomTabbedPage
     {
         public MainPage()
         {
