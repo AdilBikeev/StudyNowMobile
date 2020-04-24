3b3f75adffa5d3c5c69c9d142bacb5586d6d88e3
AdilBikeev <bikeev1999@mail.ru>
2020-04-24 18:29:51 +0300

    ОРМП-6 Испрвлена проблема с отображением навигационной панели

--- StudyNowMobileApp/StudyNowMobileApp.Android/MainActivity.cs
+++ StudyNowMobileApp/StudyNowMobileApp.Android/MainActivity.cs
@@ -6,7 +6,15 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms.Platform.Android.AppCompat;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using Android.Support.Design.Widget;
+using Android.Content;
+using StudyNowMobileApp.Views;
+using StudyNowMobileApp.Droid;
 
+[assembly: ExportRenderer(typeof(MainPage), typeof(MyTabbedPageRenderer))]
 namespace StudyNowMobileApp.Droid
 {
     [Activity(Label = "StudyNowMobileApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -28,6 +36,42 @@ namespace StudyNowMobileApp.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public class MyTabbedPageRenderer : TabbedPageRenderer
+    {
+        public MyTabbedPageRenderer(Context context) : base(context)
+        {
+
+        }
+
+        /// <summary>
+        /// Убирает скрытие иконок на навигационной панели.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null && e.NewElement != null)
+            {
+                for (int i = 0; i <= this.ViewGroup.ChildCount - 1; i++)
+                {
+                    var childView = this.ViewGroup.GetChildAt(i);
+                    if (childView is ViewGroup viewGroup)
+                    {
+                        for (int j = 0; j <= viewGroup.ChildCount - 1; j++)
+                        {
+                            var childRelativeLayoutView = viewGroup.GetChildAt(j);
+                            if (childRelativeLayoutView is BottomNavigationView)
+                            {
+                                ((BottomNavigationView)childRelativeLayoutView).ItemIconTintList = null;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
